0270c00148230e0b88c09e44fbf1257e1e4b82cd
zero <teranori@gmail.com>
2018-05-31 10:57:55 +0800

    first commit.

---
+++ tests/test.py
@@ -0,0 +1,35 @@
+
+import time
+SELENIUM_HUB = 'http://localhost:4444/wd/hub'
+
+from selenium import webdriver
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+
+driver = None
+try:
+
+    print("starting driver")
+    driver = webdriver.Remote(
+      command_executor=SELENIUM_HUB,
+      desired_capabilities=DesiredCapabilities.CHROME,
+    )
+
+    print("opening the page")
+    driver.get('https://sleepless-se.net/')
+
+    print("take a screenshot")
+    driver.save_screenshot('screenshot.png')
+
+    print("show title")
+    print(driver.title)
+
+    time.sleep(2)
+
+except:
+
+    print("error")
+
+finally:
+
+    driver.quit()
+    print("finish")
